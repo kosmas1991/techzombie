d6e92d802fd22aca689a2e20b5f6ca288117d8ff
kosmas1991 <kosmasgou@gmail.com>
2021-04-05 21:39:03 +0300

    video player at video posts finally works at every post change

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screens/homeScreen.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
     return MaterialApp(
       title: 'Techzombie',
       home: HomeScreen(),
